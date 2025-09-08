024ffd0ded6e3fb2886a215ac08b6ad5ed6b0a37
Niko Köbler <niko@n-k.de>
2025-09-08 13:23:07 +0200

    proxy options

--- tf.sh
+++ tf.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#docker run --rm -v ./terraform:/tmp/tf -v ./certs:/certs -w /tmp/tf -e HTTP_PROXY=http://proxy:3128 -e HTTPS_PROXY=http://proxy:3128 -e NO_PROXY="lb,localhost,127.0.0.0/8" --network=keycloak-workshop_default hashicorp/terraform:latest "$@"
 docker run --rm -v ./terraform:/tmp/tf -v ./certs:/certs -w /tmp/tf --network=keycloak-workshop_default hashicorp/terraform:latest "$@"
